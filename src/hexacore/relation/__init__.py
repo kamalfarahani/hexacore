7a9516624d538912c541257823407e4fe7c0f071
Kamal <kamal.farahani90@gmail.com>
2026-09-06 16:57:31 +0330

    feat(relation): include ManyToMany, OneToMany, and OneToOne in __all__ exports

--- src/hexacore/relation/__init__.py
+++ src/hexacore/relation/__init__.py
@@ -2,8 +2,14 @@
 
 from .base_relation import BaseRelation
 from .base_relation_mutation import BaseRelationMutation
+from .many_to_many import ManyToMany
+from .one_to_many import OneToMany
+from .one_to_one import OneToOne
 
 __all__ = [
     "BaseRelation",
     "BaseRelationMutation",
+    "ManyToMany",
+    "OneToMany",
+    "OneToOne",
 ]
